ad0f30dd3bafbf85c1dd3e5d970d119984158bd5
Klubok-codi <113032485+Klubok-codi@users.noreply.github.com>
2022-09-07 16:29:20 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -31,3 +31,9 @@ $mail->isHTML(true);                                  // Set email format to HTM
 $mail->Subject = 'Заявка на покупку';
 $mail->Body    = '' .$name . ' оставил заявку, его телефон ' .$phone. '<br>Почта этого пользователя: ' .$email;
 $mail->AltBody = '';
+if(!$mail->send()) {
+    echo 'Error';
+} else {
+    header('location: card-page.html');
+}
+?>
